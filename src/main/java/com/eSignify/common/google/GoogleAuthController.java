6f98baa8ac73469be4ce661a7cd037fe47830592
myungwon_seo <myungwon_seo@mf.seegene.com>
2024-11-05 17:58:15 +0900

    Google oAuth 추가

--- src/main/java/com/eSignify/common/google/GoogleAuthController.java
+++ src/main/java/com/eSignify/common/google/GoogleAuthController.java
@@ -1,33 +1,18 @@
-package com.eSignify.common.kakao;
+package com.eSignify.common.google;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class KakaoAuthController {
-
-    @Autowired
-    private KakaoAuthService kakaoAuthService;
-    
-    @Autowired
-    private KakaoSendService kakaoSendService;
-
-    
+public class GoogleAuthController {
+	
     // 확인하고 계속하기
     // https://kauth.kakao.com/oauth/authorize?client_id=afd0afadcf81f1efaf97fe02625c2963&redirect_uri=http://localhost:8080/oauth/kakao&response_type=code
-    @GetMapping("/oauth/kakao")
-    public AccessTokenResponse kakaoCallback(@RequestParam String code) throws Exception {
+    @GetMapping("/oauth/goole")
+    public String kakaoCallback(@RequestParam String code) throws Exception {
     	
-        // authorization_code를 사용해 accessToken을 발급받습니다.
-    	
-    	AccessTokenResponse	tkResponse = kakaoAuthService.getAccessToken(code);
-    	
-        kakaoSendService.getFriends(tkResponse);
-        
-        
-        return tkResponse;
+        return "123";
     }
 }
 //http://localhost:8080/oauth/google
